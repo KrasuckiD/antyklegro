1805d6bcccdee4135586c12d454e35bcd4db0fc6
KrasuckiD <darasmm@gmail.com>
2018-07-16 18:04:40 +0200

    start

---
+++ src/main/java/com/example/antyklegro/AntyklegroApplication.java
@@ -0,0 +1,12 @@
+package com.example.antyklegro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntyklegroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntyklegroApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/antyklegro/AntyklegroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.antyklegro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AntyklegroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
